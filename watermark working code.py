7b47b59029e5bf621c6f8ff4998f6f1d48974df7
swimmerbob <47184414+swimmerbob@users.noreply.github.com>
2020-01-13 20:26:46 -0600

    weekend workload
    made some stuff better will try to merge with changes done at school

--- watermark working code.py
+++ watermark working code.py
@@ -5,60 +5,45 @@ import PIL.ImageDraw
 from PIL import Image, ImageDraw
          
 
-def round_corners_one_image(original_image, percent_of_side=.3):
+def watermark_one_image(original_image):
     print("what watermaerk do you want?")
     imput = input(" ")
-    """ Rounds the corner of a PIL.Image
-    
-    original_image must be a PIL.Image
-    Returns a new PIL.Image with rounded corners, where
-    0 < percent_of_side < 1
-    is the corner radius as a portion of the shorter dimension of original_image
+    """ 
+    takes an image and pastes a wateramrk and custom text
+    left some random bits of code dont know what they do but it dosent
+    work if comented out or erased
     """
-    #set the radius of the rounded corners
+    
+    if original_image == '0crest.png':
+        original_image = original_image.resize((50,50))
+    else:
+        original_image = original_image.resize((300,300))
+        draw = ImageDraw.Draw(original_image)
+        draw.text((0, 250),imput, fill=255)
+        del draw
+        original_image = original_image.paste(img)  
+    
     width, height = original_image.size
-    radius = int(percent_of_side * min(width, height)) # radius in pixels
     
-    ###
-    #create a mask
-    ###
     
-    #start with transparent mask
+    
+    
+    #start with transparent mask 
+    #not sure what this does dont remove it doe
     rounded_mask = PIL.Image.new('RGBA', (width, height), (127,0,127,255))
-    #drawing_layer = PIL.ImageDraw.Draw(rounded_mask)
+    
     
     # Overwrite the RGBA values with A=255.
     # The 127 for RGB values was used merely for visualizing the mask
     
-    # Draw two rectangles to fill interior with opaqueness
-    '''
-    drawing_layer.polygon([(radius,0),(width-radius,0),
-                            (width-radius,height),(radius,height)],
-                            fill=(127,0,127,255))
-    drawing_layer.polygon([(0,radius),(width,radius),
-                            (width,height-radius),(0,height-radius)],
-                            fill=(127,0,127,255))
-    '''
-
-    #Draw four filled circles of opaqueness
-    #drawing_layer.ellipse((0,0, 2*radius, 2*radius), 
-                            #fill=(0,127,127,255)) #top left
-    #drawing_layer.ellipse((width-2*radius, 0, width,2*radius), 
-                            #fill=(0,127,127,255)) #top right
-    #drawing_layer.ellipse((0,height-2*radius,  2*radius,height), 
-                            #fill=(0,127,127,255)) #bottom left
-    #drawing_layer.ellipse((width-2*radius, height-2*radius, width, height), 
-                            #fill=(0,127,127,255)) #bottom right
-                         
+    
     # Uncomment the following line to show the mask
     #plt.imshow(rounded_mask)
     
-    draw = ImageDraw.Draw(original_image)
-    draw.text((0, 55),imput, fill=128)
     
-    del draw
     
     # Make the new image, starting with all transparent
+    #another instance of nessesary but wierd code
     result = PIL.Image.new('RGBA', original_image.size, (0,0,0,0))
     result.paste(original_image, (0,0), mask=rounded_mask)
     return result
@@ -70,6 +55,7 @@ def get_images(directory=None):
     Returns a 2-tuple containing 
     a list with a  PIL.Image object for each image file in root_directory, and
     a list with a string filename for each image file in root_directory
+    taken from original file
     """
     
     if directory == None:
@@ -89,12 +75,12 @@ def get_images(directory=None):
             pass # do nothing with errors tying to open non-images
     return image_list, file_list
 
-def round_corners_of_all_images(directory=None):
+def watermark_all(directory=None):
     """ Saves a modfied version of each image in directory.
     
     Uses current directory if no directory is specified. 
     Places images in subdirectory 'modified', creating it if it does not exist.
-    New image files are of type PNG and have transparent rounded corners.
+    New image files are of type PNG and have the watermarks added 
     """
     
     if directory == None:
@@ -116,45 +102,10 @@ def round_corners_of_all_images(directory=None):
         print (n)
         filename, filetype = os.path.splitext(file_list[n])
         
-        # Round the corners with default percent of radius
+        # adds watermarks
         curr_image = image_list[n]
-        new_image = round_corners_one_image(curr_image) 
+        new_image = watermark_one_image(curr_image) 
         
         # Save the altered image, suing PNG to retain transparency
         new_image_filename = os.path.join(new_directory, filename + '.png')
         new_image.save(new_image_filename)   
-def clearbackground(directory=None):
-    if directory == None:
-        directory = os.getcwd() # Use working directory if unspecified
-        
-    # Create a new directory 'modified'
-    new_directory = os.path.join(directory, 'modified')
-    try:
-        os.mkdir(new_directory)
-    except OSError:
-        pass # if the directory already exists, proceed  
-    
-    # Load all the images
-    image_list, file_list = get_images(directory)  
-
-    # Go through the images and save modified versions
-    for n in range(len(image_list)):
-        # Parse the filename
-        print (n)
-        filename, filetype = os.path.splitext(file_list[n])
-        
-        # Round the corners with default percent of radius
-        curr_image = image_list[n]
-        new_image = clearbackground1image(curr_image) 
-        
-        # Save the altered image, suing PNG to retain transparency
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename)   
-def clearbackground1image(original_image):
-    for r in range(0,500):
-        for c in range(0,500):
-            if sum(original_image[r][c])<50:
-                original_image[r][c]=[0,0,0,0]
-    result = PIL.Image.new('RGBA', original_image.size, (0,0,0,0))
-    result.paste(original_image, (0,0), mask=rounded_mask)
-    return result
